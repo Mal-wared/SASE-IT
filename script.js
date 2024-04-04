9e2f601673047a9a033bbd3d3564c585ee3118d5
Dannyle1237 <danny.le3@mavs.uta.edu>
2024-04-03 19:50:27 -0500

    Pineapple

--- script.js
+++ script.js
@@ -8,35 +8,34 @@ function call(){
     .then(function (data) {
         coursework = data;
         console.log(coursework);
-
-        var i = 0;
-        document.getElementById('lists').innerHTML = '';
-        // Display the data on client
-        for (var key in coursework){
-            // Create a new list item element'
-            var new_div = document.createElement('div');
-            var new_butt = document.createElement('button');
-            new_butt.innerHTML = "V";
-            new_butt.id = key;
-            new_butt.onclick = display_sublists;
-            var new_li = document.createElement('li');
-            new_li.innerHTML = key;
-
-            new_div.className = "list_item";
-            new_div.appendChild(new_butt);
-            new_div.appendChild(new_li);
-
-            document.getElementById('lists').appendChild(new_div);
-
-            
-        }
+        display_courses(coursework);
     })
     .catch(function (error) {
         console.log('Error:', error);
     });
-
 }
 
+function display_courses(coursework){
+    var i = 0;
+    document.getElementById('lists').innerHTML = '';
+    // Display the data on client
+    for (var key in coursework){
+        // Create a new list item element'
+        var new_div = document.createElement('div');
+        var new_butt = document.createElement('button');
+        new_butt.innerHTML = "V";
+        new_butt.id = key;
+        new_butt.onclick = display_sublists;
+        var new_li = document.createElement('li');
+        new_li.innerHTML = key;
+
+        new_div.className = "list_item";
+        new_div.appendChild(new_butt);
+        new_div.appendChild(new_li);
+
+        document.getElementById('lists').appendChild(new_div);
+    }
+}
 
 function display_sublists(e){
     for (var key in coursework[e.target.id]){
@@ -55,4 +54,56 @@ function display_sublists(e){
 
         e.target.parentNode.appendChild(new_div);
     }
+}
+
+function add_course(){
+    console.log("Adding course");
+    new_course = document.getElementById("new_course").value;
+    json_course = {
+        "action": "add_course",
+        "course_name": new_course
+    }
+    send_post_request(json_course)
+}
+
+function add_homework(){
+    console.log("Adding homework");
+    new_homework = document.getElementById("new_homework").value;
+    new_date = document.getElementById("new_date").value;
+
+    json_homework = {
+        "action": "add_homework",
+        "course_name": "phys_2111",
+        "hw_name": new_homework,
+        "date": new_date
+    }
+    send_post_request(json_homework)
+}
+function add_exam(){
+    console.log("Adding Exam");
+    new_exam = document.getElementById("new_exam").value;
+    new_examdate= document.getElementById("new_examdate").value;
+
+    json_exam ={
+        "action": "add_exam",
+        "course_name": "phys_2111",
+        "ex_name": new_exam,
+        "ex_date": new_examdate
+    }
+    send_post_request(json_exam)
+}
+
+function send_post_request(data){
+    fetch('http://127.0.0.1:5000/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        display_courses(data);
+    })
 }
