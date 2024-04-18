d4c11bf8d7d9e0b5ad052bec1a094e1daa9726e1
Mal-wared <76673485+Mal-wared@users.noreply.github.com>
2024-04-17 19:09:15 -0500

    Added data to database and revamped main page to match python dictionary

--- backend/add_to_db.py
+++ backend/add_to_db.py
@@ -12,7 +12,7 @@ new_user = User(username='john_doe', password='password123', email='john@example
 
 # Create a new course
 new_course = Course(coursename='CSE 3318', user_id=1)
-new_homework = Homework(title='Homework 14.7 Non-Linear Equations', duedate='2021-02-01', course_id=2)
+new_homework = Homework(title='Homework 14.8 Super-Linear Equations', duedate='2023-02-01', course_id=2)
 new_quiz = Quiz(title='Quiz 1', date='2021-02-03', course_id=2)
 
 print(new_course.id)
@@ -20,7 +20,7 @@ print(new_course.id)
 
 
 # Add the new user to the session
-session.add(new_quiz)
+session.add(new_homework)
 
 # Commit the session to save the changes to the database
 session.commit()
